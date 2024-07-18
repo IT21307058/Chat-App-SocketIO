b1db2134ce8c79176d69cafd04d63183341a2307
IT21307058 <IT21307058@my.sliit.lk>
2024-07-18 23:48:18 +0530

    changes commited

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "primary":"#1476ff",
+        "primary-light": '#f3f5ff',
+        "light": "#f9faff",
+        "secondary":"#eeeeee",
+        "white":"#ffff"
+  
+      }
+    },
+  },
+  plugins: [],
+}
+
